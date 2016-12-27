cd996a70a0d6d3303a8213c5ce9cec01d3de96b2
Yo Mitani <mitani.yo1995@gmail.com>
2016-12-27 21:09:35 +0900

    fix argument order

--- PowerPC/emit.ml
+++ PowerPC/emit.ml
@@ -60,7 +60,7 @@ let reg r =
 
 let load_label r label = (* TO BE DECIDED *)
   let r' = reg r in
-  Printf.sprintf "\taddi\t%s %s %s\n" r' "r0" label
+  Printf.sprintf "\taddi\t%s %s %s\n" "r0" r' label
 (*  Printf.sprintf
     "\tlis\t%s, ha16(%s)\n\taddi\t%s, %s, lo16(%s)\n"
     r' label r' r' label*)
