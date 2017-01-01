95779e9dd4951f15ae1bd2df571e0d8991389698
Yo Mitani <mitani.yo1995@gmail.com>
2017-01-01 17:30:50 +0900

    write read and print

---
+++ test/iotest.ml
@@ -0,0 +1,11 @@
+let x = read_int () in
+let y = read_int () in
+let z = x + y in
+let a = print_int z in
+let b = print_char 32 in
+let s = read_float () in
+let t = read_float () in
+let u = s +. t in
+let d = print_char 10 in
+()
+
