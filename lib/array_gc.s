99886ace201d2a90fe62af008999c41e37dd65fa
Yo Mitani <mitani.yo1995@gmail.com>
2017-01-31 20:15:29 +0900

    add array_gc.s

---
+++ lib/array_gc.s
@@ -0,0 +1,40 @@
+min_caml_create_array:
+# reg_type : r22
+andi r22 r3 1
+sll r1 r4 3
+addi r4 r4 3
+sw r4 r27 0
+addi r4 r4 1
+addi r0 r5 4
+create_array_loop:
+swo r2 r27 r5
+addi r5 r5 4
+swo r3 r27 r5
+addi r5 r5 4
+sub r5 r4 r25
+beq r25 r0 create_array_return
+j create_array_loop
+
+create_array_return:
+add r27 r4 r27
+jr r31
+
+min_caml_create_float_array:
+addi r0 r3 2
+sll r1 r4 3
+addi r4 r4 3
+sw r4 r27 0
+addi r4 r4 1
+addi r0 r5 4
+create_float_array_loop:
+swoc1 f0 r27 r5
+addi r5 r5 4
+swo r3 r27 r5
+addi r5 r5 4
+sub r5 r4 r25
+beq r25 r0 create_float_array_return
+j create_float_array_loop
+
+create_float_array_return:
+add r27 r4 r27
+jr r31
