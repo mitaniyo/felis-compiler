4c498964252376023372ace0164444dd09b9429b
Yo Mitani <mitani.yo1995@gmail.com>
2017-01-31 19:41:32 +0900

    implement gc.s

--- gc/gc.s
+++ gc/gc.s
@@ -83,8 +83,8 @@ j gc_copyall_nextloop
 gc_copy_float:
 lwc1 r3 f31 0
 swc1 f31 r4 0
-lwc1 r3 r7 4
-sc r7 r4 4
+lw r3 r7 4
+sw r7 r4 4
 j gc_copyall_nextloop
 
 gc_copy_adr:
@@ -94,85 +94,32 @@ lw r7 r8 0
 andi r8 r9 3
 # if r9 = 0 then this address is copied
 beq r9 r0 gc_adr_copied
+sw r8 r27 0
+sw r27 r7 0
 # save variables and call copyall
+sw r3 r30 0
+sw r4 r30 4
+sw r5 r30 8
+sw r6 r30 12
+# from r7 to reg_hp
+# sz = r8 + 1
+addi r8 r5 1
+addi r7 r3 0
+addi r27 r4 0
+add r27 r5 r27
+sw r31 r30 16
+addi r30 r30 20
+jal gc_copyall
+addi r30 r30 -20
+lw r30 r31 16
+lw r30 r6 12
+lw r30 r5 8
+lw r30 r4 4
+lw r30 r3 0
+j gc_copyall_nextloop
 
 gc_adr_copied:
 sw r8 r4 0
 lw r3 r7 4
 sw r7 r4 4
 j gc_copyall_nextloop
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-min_caml__gc:
-# r1 := current reg_sp
-addi r30 r1 0
-# reg_hp is already changed to new one
-# reg_hp = r27
-# for r2 = 0 to r1
-addi r0 r2 0
-gc_root_loop:
-sub r1 r2 r25
-beq r25 r0 gc_return
-lw r2 r3 4
-# 0 -> this is not heap address
-beq r3 r0 gc_next_root_loop
-lw r2 r3 0
-# r3 := M[r2], root
-# r1, r2 will not updated in dfs
-sw r31 r30 0 # save link register
-addi r30 r30 4 # do not manage stack type info during gc
-jal gc_dfs
-addi r30 r30 -4
-lw r30 r31 0
-gc_next_root_loop:
-addi r2 r2 8
-j gc_root_loop
-
-gc_return:
-jr r31
-
-gc_copyall:
-# r3 : address
-
-
-gc_dfs:
-# r3 : reachable address
-lw r3 r4 4 # r4 := type of r3
-# non address
-andi r4 r25 1
-
-
-andi r3 r25 3
-beq r25 r0 restore_copied_address
-# this value is not copied
-# allocate the same size of memory to new heap
-# size = r3 + 1
-sw r3 r27 0
-sw r27 r2 0
-add r27 r3 r27
-addi r27 r27 1
-# copy each element
