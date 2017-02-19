9c7b62db74b3131b23cf190fc8e3a2d45fdd5762
Yo Mitani <mitani.yo1995@gmail.com>
2017-02-19 18:20:15 +0900

    change sin/cos

--- lib/gentrig.cpp
+++ lib/gentrig.cpp
@@ -35,39 +35,46 @@ Real sin_coe(int i){
 int main(){
 	char ch[10];
 	scanf("%s", ch);
-	if(ch[0] == 'c'){
+/*	if(ch[0] == 'c'){
 		printf("\tlet xx = x *. x in\n");
-		printf("\tlet res20 = (%.40f) in\n", cos_coe(20));
-		for(int i = 18; i >= 0; i -= 2){
-			printf("\tlet res%d = res%d *. xx +. (%.40f) in\n", i, i + 2, cos_coe(i));
+		printf("\tlet res32 = (%.40f) in\n", cos_coe(20));
+		for(int i = 30; i >= 0; i -= 2){
+			printf("\tlet res%d = res%d *. x2 +. (%.40f) in\n", i, i + 2, cos_coe(i));
 		}
 		printf("\tres0\n");
 	}else{
 		printf("\tlet xx = x *. x in\n");
-		printf("\tlet res19 = (%.40f) in\n", sin_coe(19));
-		for(int i = 17; i >= 1; i -= 2){
+		printf("\tlet res31 = (%.40f) in\n", sin_coe(19));
+		for(int i = 29; i >= 1; i -= 2){
 			printf("\tlet res%d = res%d *. xx +. (%.40f) in\n", i, i + 2, sin_coe(i));
 		}
 		printf("res1 *. x\n");
-	}/*
-	if(ch[0] == 'c'){
-		printf("\tlet tmp0 = 1.0 in\n");
-		for(int i = 2; i <= 18; i += 2){
-			printf("\tlet tmp%d = -. (tmp%d /. %d.0 /. %d.0) in\n", i, i - 2, i, i - 1);
-		}
-		printf("\tlet res20 = 0.0 in\n");
-		for(int i = 18; i >= 0; i -= 2){
-			printf("\tlet res%d = res%d *. x *. x +. tmp%d in\n", i, i + 2, i);
-		}
-	}else{
-		printf("\tlet tmp1 = 1.0 in\n");
-		for(int i = 3; i <= 19; i += 2){
-			printf("\tlet tmp%d = -.(tmp%d /. %d.0 /. %d.0) in\n", i, i - 2, i, i - 1);
-		}
-		printf("\tlet res21 = 0.0 in\n");
-		for(int i = 19; i >= 1; i -= 2){
-			printf("\tlet res%d = res%d *. x *. x +. tmp%d in\n", i, i + 2, i);
-		}
 	}*/
+
+	if(ch[0] == 'c'){
+		printf("\tlet xx = x *. x in\n");
+		printf("\tlet x4 = xx *. xx in\n");
+		printf("\tlet pres36 = (%.40f) in\n", cos_coe(46));
+		for(int i = 32; i >= 0; i -= 4){
+			printf("\tlet pres%d = pres%d *. x4 +. (%.40f) in\n", i, i + 4, cos_coe(i));
+		}
+		printf("\tlet nres34 = (%.40f) in\n", cos_coe(34));
+		for(int i = 30; i >= 2; i -= 4){
+			printf("\tlet nres%d = nres%d *. x4 +. (%.40f) in\n", i, i + 4, cos_coe(i));
+		}
+		printf("\tpres0 +. nres2 *. xx\n");
+	}else{
+		printf("\tlet xx = x *. x in\n");
+		printf("\tlet x4 = xx *. xx in\n");
+		printf("\tlet nres35 = (%.40f) in\n", sin_coe(35));
+		for(int i = 31; i >= 3; i -= 4){
+			printf("\tlet nres%d = nres%d *. x4 +. (%.40f) in\n", i, i + 4, sin_coe(i));
+		}
+		printf("\tlet pres33 = (%.40f) in\n", sin_coe(33));
+		for(int i = 29; i >= 1; i -= 4){
+			printf("\tlet pres%d = pres%d *. x4 +. (%.40f) in\n", i, i + 4, sin_coe(i));
+		}
+		printf("\tx *. (pres1 +. nres3 *. xx)\n");
+	}
 	return 0;
 }
